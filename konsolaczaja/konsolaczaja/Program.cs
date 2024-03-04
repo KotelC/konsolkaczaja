2105491bd3d5cd1ff346954ef76c48d3042dca11
KotelC <94064339+KotelC@users.noreply.github.com>
2024-03-04 10:33:09 +0100

    Add files via upload

---
+++ konsolaczaja/konsolaczaja/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Przeliczanie
+{
+    static void Main()
+    {
+        int n = 100;
+        bool[] liczby = new bool[n + 1];
+
+        Przelicz(liczby, n);
+
+        Console.WriteLine("Liczby pierwsze w przedziale do 100");
+
+        Wypisz(liczby);
+    }
+
+
+    static void Przelicz(bool[] liczby, int n)
+    {
+        for (int i = 0; i < n; i++)
+
+        {
+            liczby[i] = true;
+        }
+
+
+        for (int p = 2; p * p <= n;  p++)
+
+        {
+            if (liczby[p] == true)
+
+
+                for (int i = p * p; i <= n; i += p)
+                {
+                    liczby[i] = false;
+                }
+
+        }
+    }
+
+
+
+    static void Wypisz(bool[] liczby)
+    {
+        for (int p = 2; p < liczby.Length; p++)
+        {
+            if (liczby[p] == true)
+            {
+                Console.Write(p + " ");
+
+            }
+        }
+        Console.WriteLine();
+                }
+
+
+}
